1e8742f282fec0f6db1ce9555e866e15765bdb13
Javaphite <javaphite@gmail.com>
2018-06-29 20:19:18 +0300

    StorageService refactoring and cleaning. StorageService tests removed due to class simplicity.

--- src/main/java/home/javaphite/beholder/storage/StorageService.java
+++ src/main/java/home/javaphite/beholder/storage/StorageService.java
@@ -1,36 +1,40 @@
 package home.javaphite.beholder.storage;
 
 import home.javaphite.beholder.storage.accessors.Accessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-// UNDER CONSTRUCTION
+/**
+ * Service intended to store prepared data in remote storage.
+ */
 public class StorageService {
-   final Queue<Map<String, Object>> storageQueue = new ConcurrentLinkedQueue<>();
-   private Accessor<Map<String, Object>> storageAccessor;
+   private static final Logger LOG = LoggerFactory.getLogger(StorageService.class);
+   private Queue<Map<String, Object>> storageQueue = new ConcurrentLinkedQueue<>();
+   private Accessor<Map<String, Object>> accessor;
 
+   public StorageService(Accessor<Map<String, Object>> accessor) {
+      this.accessor = accessor;
+   }
+
+    /**
+     * Queues atomic portion of data to be stored in future.
+     * @param data atomic portion of data.
+     */
    public void queue(Map<String, Object> data) {
+       LOG.debug("Adding data to queue {}", data);
        storageQueue.add(data);
     }
 
-   public void store() {
-        while (!storageQueue.isEmpty())
-            storeNext();
-    }
-
-    private void storeNext() {
-        Map<String, Object> data = storageQueue.poll();
-        storageAccessor.push(data);
-    }
-
-    public void setStorageAccessor(Accessor<Map<String, Object>> accessor) {
-        if (null != accessor) {
-            this.storageAccessor = accessor;
-        }
-        else {
-            throw new IllegalArgumentException("Accessor couldn't be null.");
-        }
-    }
+    /**
+     * Stores all currently queued data in remote storage.
+     */
+    public void store() {
+       LOG.info("Storing data to {}...", accessor);
+       storageQueue.forEach(accessor::push);
+       LOG.info("Storing data to {} completed!", accessor);
+   }
 }
