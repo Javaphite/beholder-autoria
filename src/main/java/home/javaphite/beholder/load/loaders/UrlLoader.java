763420ea806571deb66928fcb6b10b081e9b7df1
Javaphite <javaphite@gmail.com>
2018-06-30 20:47:28 +0300

    And little more cleaning to UrlLoader.

--- src/main/java/home/javaphite/beholder/load/loaders/UrlLoader.java
+++ src/main/java/home/javaphite/beholder/load/loaders/UrlLoader.java
@@ -20,46 +20,42 @@ public class UrlLoader{
     }
 
     public String load() {
-        String result;
        try ( Scanner scanner = new Scanner(setupConnection().getInputStream()) ) {
             StringBuilder builder = new StringBuilder();
             scanner.useDelimiter("(?m)$"); //matches position after end of each line (multi-line mode)
             scanner.forEachRemaining(builder::append);
-            result = Objects.toString(builder);
+            return Objects.toString(builder);
         }
         catch (IOException connectionError) {
             LOG.error("Connection error: ", connectionError);
             throw new UncheckedIOException(connectionError);
         }
-        return result;
     }
 
     private URL getUrl(String link) {
-        URL url;
         try {
             LOG.debug("Trying to form URL object for {}", link);
-            url = new URL(link);
+            URL url = new URL(link);
             LOG.debug("URL object for {} successfully created!", link);
+            return url;
         }
         catch (MalformedURLException badUrlException) {
-            LOG.error(" String {} is not appropriate URL link: {}", link, badUrlException);
+            LOG.error("String {} is not appropriate URL link: {}", link, badUrlException);
             throw new IllegalArgumentException(badUrlException);
         }
-        return url;
     }
 
     private URLConnection setupConnection() {
         URL url = getUrl(source);
-        URLConnection connection;
         try {
             LOG.info("Trying to establish connection to {}", url);
-            connection = url.openConnection();
+            URLConnection connection = url.openConnection();
             LOG.info("Connection to {} successfully established!", url);
+            return connection;
         }
         catch (IOException connectionError) {
             LOG.error("Connection establishment failed:", connectionError);
             throw new UncheckedIOException(connectionError);
         }
-        return connection;
     }
 }
