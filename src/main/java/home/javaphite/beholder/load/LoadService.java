f419b944be1ecfe9ebed7a9b57d135876a187528
Javaphite <javaphite@gmail.com>
2018-06-30 20:40:24 +0300

    LoadService tests removed as unnecessary after refactoring tested class. Minor changes in LoadService.

--- src/main/java/home/javaphite/beholder/load/LoadService.java
+++ src/main/java/home/javaphite/beholder/load/LoadService.java
@@ -6,7 +6,6 @@ public class LoadService {
     /**
      * Loads text content or remote resource by URL.
      * @param link URL link to resource.
-     * @return text content of resource.
      */
     public String loadContent(String link) {
        UrlLoader loader = new UrlLoader(link);
